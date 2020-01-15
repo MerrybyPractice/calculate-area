239f9f998cc39368b02ffb1a98f1cda80a60b6e3
Merry Cimakasky <mkcimakasky@gmail.com>
2020-01-15 10:39:06 -0800

    basic tests for circle, square, triangle

---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+    @Test
+    public void areaOfTriangleWithSidesOneTest(){
+        Triangle test = new Triangle(1);
+
+        assertEquals(0.43, test.areaOf(), "A Triangle with sides of 1 will have an area of 0.43.");
+
+    }
+
+    @Test
+    public void areaOfTriangleWithSidesZeroTest(){
+        Triangle test = new Triangle(0);
+
+        assertEquals(0, test.areaOf(), "A Triangle with sides of 0 will have an area of 0.");
+    }
+
+    @Test
+    public void invalidTriangleSidesStringTest(){
+        fail(new Triangle('a'));
+}
